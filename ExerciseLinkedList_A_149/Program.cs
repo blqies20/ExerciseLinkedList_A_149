69d34ee8359990e680c7281ee8b3f006af997eef
ACER <ACER@DESKTOP-QH5VR0V>
2022-11-23 16:06:09 +0700

    add console write rollnumber in traverse

--- ExerciseLinkedList_A_149/Program.cs
+++ ExerciseLinkedList_A_149/Program.cs
@@ -51,8 +51,13 @@ namespace ExerciseLinkedList_A_149
                 Node currentNode;
                 currentNode = LAST.next;
                 while (currentNode != LAST)
-                { }
+                {
+                    Console.Write(currentNode.rollNumber + "  " + currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + "  " + LAST.name + "\n");
             }
         }
+
     }
 }
