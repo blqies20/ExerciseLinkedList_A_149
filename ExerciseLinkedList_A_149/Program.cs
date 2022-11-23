d783eadf915312e0c86dbfb6660a5b2549d6a538
ACER <ACER@DESKTOP-QH5VR0V>
2022-11-23 15:42:44 +0700

    add class node

--- ExerciseLinkedList_A_149/Program.cs
+++ ExerciseLinkedList_A_149/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseLinkedList_A_149
+{
+    class Node
+    {
+        /*Nodes class for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+        public Node prev;
+    }
+   
+}
