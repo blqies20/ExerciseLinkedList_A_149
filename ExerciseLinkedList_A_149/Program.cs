ce8cc701a4235567cbf58cce2d74c866b759e651
ACER <ACER@DESKTOP-QH5VR0V>
2022-11-23 16:26:14 +0700

    add object circular list

--- ExerciseLinkedList_A_149/Program.cs
+++ ExerciseLinkedList_A_149/Program.cs
@@ -70,7 +70,22 @@ namespace ExerciseLinkedList_A_149
             CircularList obj = new CircularList();
             while (true)
             {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all the records in the list");
+                    Console.WriteLine("4. Search for a record in the list");
+                    Console.WriteLine("5. Display the first record in the list");
+                    Console.WriteLine("6. Exit");
+                    Console.Write("\nEnter your choice (1-6): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
 
+                    }
+                }
             }
         }
     }
