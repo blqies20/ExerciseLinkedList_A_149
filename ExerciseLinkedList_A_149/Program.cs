9c98c339258d026a04a53cb76830356e51915c0d
ACER <ACER@DESKTOP-QH5VR0V>
2022-11-23 16:13:23 +0700

    add public void firstnode

--- ExerciseLinkedList_A_149/Program.cs
+++ ExerciseLinkedList_A_149/Program.cs
@@ -58,6 +58,12 @@ namespace ExerciseLinkedList_A_149
                 Console.Write(LAST.rollNumber + "  " + LAST.name + "\n");
             }
         }
-
+        public void firstNode()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+                Console.WriteLine("\nThe first record in the list is: \n\n " + LAST.next.rollNumber + "  " + LAST.next.name);
+        }
     }
 }
